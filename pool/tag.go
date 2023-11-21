b0f1b5558e4046b75205fb90630e3d05f3f088b5
zuiwuchang <zuiwuchang@gmail.com>
2023-11-21 02:55:12 +0000

    pool get default

--- pool/tag.go
+++ pool/tag.go
@@ -7,9 +7,15 @@ var pools = make(map[string]*Pool)
 func Get(tag string, cap, len int) *Pool {
 	found, ok := pools[tag]
 	if !ok {
-		if cap < 1 {
+		if cap == 0 {
 			cap = 1024 * 128
+		} else if cap < 0 {
+			cap = 1024 * 32
+		}
+		if len == 0 {
 			len = 8 * 10
+		} else if len < 0 {
+			len = 0
 		}
 		found = New(cap, len)
 		pools[tag] = found
